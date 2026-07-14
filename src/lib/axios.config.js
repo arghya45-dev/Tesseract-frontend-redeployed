722827bf1496dc821bf2267805ed7b4b0cd7c268
arghya45-dev <chakrabortyarghya11@gmail.com>
2026-07-14 16:31:19 +0530

    my backend

--- src/lib/axios.config.js
+++ src/lib/axios.config.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://tesseract-backend-j00k.onrender.com/api",  //Niladri's backend
-  // mine : https://tesseract-backend-redeployed.onrender.com
+  baseURL: "https://tesseract-backend-redeployed.onrender.com/api",
   // baseURL: "http://localhost:5001/api",
   withCredentials: true,
 });
