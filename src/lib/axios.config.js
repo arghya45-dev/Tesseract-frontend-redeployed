aa244049f484a2a9827a3fd5e5bef29ba5f42f44
arghya45-dev <chakrabortyarghya11@gmail.com>
2026-07-14 15:44:46 +0530

    INITIAL FRONTEND SETUP

--- src/lib/axios.config.js
+++ src/lib/axios.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://tesseract-backend-j00k.onrender.com/api",
+  baseURL: "https://tesseract-backend-redeployed.onrender.com",
   // baseURL: "http://localhost:5001/api",
   withCredentials: true,
 });
